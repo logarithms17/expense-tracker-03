770eed568a1ac9a8ecd57f8c5935d26eef36a73a
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-08-25 18:28:09 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/event-tracker-03'>
+        <BrowserRouter basename='/expense-tracker-03'>
           <App />
         </BrowserRouter>
       </PersistGate>
